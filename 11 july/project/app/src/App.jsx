51db1c19d3c609300d08416120676f1c8d7b4591
ujjwal1309 <ujjwalsharma1309@gmail.com>
2023-07-11 22:53:25 +0530

    done

---
+++ 11 july/project/app/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+//component
+
+function App() {
+  return (
+    <div>
+      <h1>app</h1>
+      <h2>Heloo lol</h2>
+    </div>
+  );
+}
+
+export default App;
