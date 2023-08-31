b775a3e73a0037000e56fa602e5d5ca205f304b9
ujjwal1309 <ujjwalsharma1309@gmail.com>
2023-08-31 15:51:49 +0530

    changes made to admin page

--- 22_June/1.js
+++ 22_June/1.js
@@ -12,6 +12,10 @@ function foo(arr, n) {
 
 // this code is gonna take O(n^2)
 
+//All things are achievable if i put the effort and time into it but how I have to get into job and i don;t know if i can be able to . What's the problem i am not disabled at all i can do anything which things are keep me away at focus , pokemon unite . If this happens how i am going to improve my lifestyle I lnow that i can do it. I want to get a ggo d job that's all my dream is to become a billionaire not a rotten shit that lies all day on bed.
+
+
+
 function runProgram(input) {
   // Write code here
   input = input.trim().split("\n");
@@ -47,3 +51,4 @@ if (process.env.USERNAME === "ujjwa") {
 }
 //waise kl maine Re/Member dekhi
 //Overall unique story thi bs thodi ending bakwaas thi pr maja aya
+
